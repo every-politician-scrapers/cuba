b3d7a6798f6944041ae4e3abf48e5d55724e2462
Tony Bowden <tony@tmtm.com>
2022-10-06 14:44:47 +0300

    Head of Radio+TV

---
+++ sources/wpoffice/ministers/radio-tv/scraper.rb
@@ -0,0 +1,37 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator WikidataIdsDecorator::Links
+
+  def holder_entries
+    noko.xpath("//h2[.//span[contains(.,'Radio y Televisión')]][1]//following-sibling::ul[1]//li")
+  end
+
+  class Officeholder < OfficeholderNonTableBase
+    def item
+      name_link.attr('wikidata') if name_link
+    end
+
+    def itemLabel
+      name_link ? name_link.text.tidy : noko.text.split(/\d/).last.tidy
+    end
+
+    def raw_combo_date
+      noko.text.gsub(itemLabel, '').gsub(/^ab /, 'seit ').gsub(/seit (.*?) /, '\1 - ').tidy
+    end
+
+    private
+
+    def name_link
+      noko.at_css('a')
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
